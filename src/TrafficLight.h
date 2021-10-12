152eca628f5c0e887e9283d02d7e7aa5d5c97d5e
= <benemichel_code@posteo.de>
2021-10-12 17:42:12 +0200

    FP.1

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -24,23 +24,36 @@ private:
     
 };
 
-// FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject. 
+// FP.1 XXX : Define a class „TrafficLight“ which is a child class of TrafficObject. 
 // The class shall have the public methods „void waitForGreen()“ and „void simulate()“ 
 // as well as „TrafficLightPhase getCurrentPhase()“, where TrafficLightPhase is an enum that 
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
 // Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
 
-class TrafficLight
+enum class TrafficLightPhase {
+    red,
+    green
+};
+
+class TrafficLight : TrafficObject
 {
 public:
     // constructor / desctructor
+    TrafficLight();
+    ~TrafficLight();
 
     // getters / setters
 
+
     // typical behaviour methods
+    void waitForGreen();
+    void simulate();
+    TrafficLightPhase getCurrentPhase();
 
 private:
     // typical behaviour methods
+    void cycleTroughPhases();
+    TrafficLightPhase _currentPhase;
 
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
